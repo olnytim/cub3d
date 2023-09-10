594bb8e675950e4021bbb985283b2d19756e4a8d
olnytim <yearagotomorrow@gmail.com>
2023-09-10 20:36:21 +0400

    reading documentation

---
+++ hf/cub3d.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgalyaut <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/10 20:12:04 by tgalyaut          #+#    #+#             */
+/*   Updated: 2023/09/10 20:12:13 by tgalyaut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D.H
+# define CUB3D.H
+// set the sizes of map and screen
+# define maW
+# define maH
+# define scW
+# define scH
+// define structs
+
+// list of functions
+
+#endif
