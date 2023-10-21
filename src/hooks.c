c9413e3f949533053611faa1408accb9a31e2090
olnytim <yearagotomorrow@gmail.com>
2023-10-21 21:27:19 +0400

    Lyuda lovi gotoviy variant. Ne rabotaet tol'ko mysh

--- src/hooks.c
+++ src/hooks.c
@@ -13,138 +13,76 @@
 #include "../include/cub3D.h"
 #include <cub3D.h>
 
-void	ft_camera_move(int keycode, t_game *game, t_player *player)
+static void	ft_turn_left_right(t_game *game, t_player *player, int i)
 {
 	double	old_dir_x;
 	double	old_plane_x;
 
 	old_dir_x = player->dir_x;
 	old_plane_x = player->plane_x;
-	if (keycode == LEFT || keycode == LEFT_L)
-	{
-		player->dir_x = player->dir_x * cos(-player->rot_speed)
-			- player->dir_y * sin(-player->rot_speed);
-		player->dir_y = old_dir_x * sin(-player->rot_speed)
-			+ player->dir_y * cos(-player->rot_speed);
-		player->plane_x = player->plane_x * cos(-player->rot_speed)
-			- player->plane_y * sin(-player->rot_speed);
-		player->plane_y = old_plane_x * sin(-player->rot_speed)
-			+ player->plane_y * cos(-player->rot_speed);
-		ft_raycasting(game);
-	}
-	if (keycode == RIGHT || keycode == RIGHT_L)
-	{
-		player->dir_x = player->dir_x * cos(player->rot_speed)
-			- player->dir_y * sin(player->rot_speed);
-		player->dir_y = old_dir_x * sin(player->rot_speed)
-			+ player->dir_y * cos(player->rot_speed);
-		player->plane_x = player->plane_x * cos(player->rot_speed)
-			- player->plane_y * sin(player->rot_speed);
-		player->plane_y = old_plane_x * sin(player->rot_speed)
-			+ player->plane_y * cos(player->rot_speed);
-		ft_raycasting(game);
-	}
+	player->dir_x = player->dir_x * cos(player->rot_speed * i)
+		- player->dir_y * sin(player->rot_speed * i);
+	player->dir_y = old_dir_x * sin(player->rot_speed * i)
+		+ player->dir_y * cos(player->rot_speed * i);
+	player->plane_x = player->plane_x * cos(player->rot_speed * i)
+		- player->plane_y * sin(player->rot_speed * i);
+	player->plane_y = old_plane_x * sin(player->rot_speed * i)
+		+ player->plane_y * cos(player->rot_speed * i);
+	ft_raycasting(game);
 }
 
-// static void	ft_move(t_game *game, int case)
-// {
-// 	if (map->map[(int)player->pos_y][(int)(player->pos_x
-// 			+ player->dir_x * player->move_speed)] == '0')
-// 		player->pos_x += player->dir_x * player->move_speed;
-// 	if (map->map[(int)(player->pos_y + player->dir_y
-// 			* player->move_speed)][(int)player->pos_x] == '0')
-// 		player->pos_y += player->dir_y * player->move_speed;
-// 	ft_raycasting(game);
-// }
+static void	ft_move_forward_backward(t_game *game, t_player *player, int i)
+{
+	if (game->map->map[(int)player->pos_y][(int)(player->pos_x
+		+ i * player->dir_x * player->move_speed)] == '0')
+		player->pos_x += i * player->dir_x * player->move_speed;
+	if (game->map->map[(int)(player->pos_y + i * player->dir_y
+			* player->move_speed)][(int)player->pos_x] == '0')
+		player->pos_y += i * player->dir_y * player->move_speed;
+	ft_raycasting(game);
+}
+
+static void	ft_move_left_right(t_game *game, t_player *player, int i, int j)
+{
+	if (game->map->map[(int)player->pos_y][(int)(player->pos_x
+		+ i * player->dir_y * player->move_speed)] == '0')
+		player->pos_x += i * player->dir_y * player->move_speed;
+	if (game->map->map[(int)(player->pos_y - j * player->dir_x
+			* player->move_speed)][(int)player->pos_x] == '0')
+		player->pos_y -= j * player->dir_x * player->move_speed;
+	ft_raycasting(game);
+}
 
 int	ft_hook(int keycode, t_game *game)
 {
-	t_player	*player;
-
-	player = game->player;
 	if (keycode == ESC || keycode == ESC_L)
 		ft_endgame(game);
 	if (keycode == W || keycode == UP || keycode == W_L || keycode == UP_L)
-	{
-		if (game->map->map[(int)player->pos_y][(int)(player->pos_x
-			+ player->dir_x * player->move_speed)] == '0')
-			player->pos_x += player->dir_x * player->move_speed;
-		if (game->map->map[(int)(player->pos_y + player->dir_y
-				* player->move_speed)][(int)player->pos_x] == '0')
-			player->pos_y += player->dir_y * player->move_speed;
-		ft_raycasting(game);
-	}
+		ft_move_forward_backward(game, game->player, 1);
 	if (keycode == S || keycode == DOWN || keycode == S_L || keycode == DOWN_L)
-	{
-		if (game->map->map[(int)player->pos_y][(int)(player->pos_x
-			+ player->dir_x * player->move_speed)] == '0')
-			player->pos_x -= player->dir_x * player->move_speed;
-		if (game->map->map[(int)(player->pos_y + player->dir_y
-				* player->move_speed)][(int)player->pos_x] == '0')
-			player->pos_y -= player->dir_y * player->move_speed;
-		ft_raycasting(game);
-	}
+		ft_move_forward_backward(game, game->player, -1);
 	if (keycode == A || keycode == A_L)
-	{
-		if (game->map->map[(int)player->pos_y][(int)(player->pos_x
-			+ player->dir_y * player->move_speed)] == '0')
-			player->pos_x += player->dir_y * player->move_speed;
-		if (game->map->map[(int)(player->pos_y - player->dir_x
-				* player->move_speed)][(int)player->pos_x] == '0')
-			player->pos_y -= player->dir_x * player->move_speed;
-		ft_raycasting(game);
-	}
+		ft_move_left_right(game, game->player, 1, -1);
 	if (keycode == D || keycode == D_L)
-	{
-		if (game->map->map[(int)player->pos_y][(int)(player->pos_x
-			- player->dir_y * player->move_speed)] == '0')
-			player->pos_x -= player->dir_y * player->move_speed;
-		if (game->map->map[(int)(player->pos_y + player->dir_x
-				* player->move_speed)][(int)player->pos_x] == '0')
-			player->pos_y += player->dir_x * player->move_speed;
-		ft_raycasting(game);
-	}
-	ft_camera_move(keycode, game, game->player);
+		ft_move_left_right(game, game->player, -1, 1);
+	if (keycode == LEFT || keycode == LEFT_L)
+		ft_turn_left_right(game, game->player, -1);
+	if (keycode == RIGHT || keycode == RIGHT_L)
+		ft_turn_left_right(game, game->player, 1);
 	return (0);
 }
 
 int	ft_mouse_hook(int button, int x, int y, t_game *game)
 {
-	double	old_dir_x;
-	double	old_plane_x;
-	t_player	*player;
-
 	(void)y;
-	(void)game;
+	(void)x;
 	(void)button;
-	player = game->player;
+	(void)game;
 	mlx_mouse_hide();
-	old_dir_x = player->dir_x;
-	old_plane_x = player->plane_x;
 	if (x < SCREEN_WIDTH / 2)
-	{
-		player->dir_x = player->dir_x * cos(-player->rot_speed)
-			- player->dir_y * sin(-player->rot_speed);
-		player->dir_y = old_dir_x * sin(-player->rot_speed)
-			+ player->dir_y * cos(-player->rot_speed);
-		player->plane_x = player->plane_x * cos(-player->rot_speed)
-			- player->plane_y * sin(-player->rot_speed);
-		player->plane_y = old_plane_x * sin(-player->rot_speed)
-			+ player->plane_y * cos(-player->rot_speed);
-		ft_raycasting(game);
-	}
+		ft_turn_left_right(game, game->player, -1);
 	else if (x > SCREEN_WIDTH / 2)
-	{
-		player->dir_x = player->dir_x * cos(player->rot_speed)
-			- player->dir_y * sin(player->rot_speed);
-		player->dir_y = old_dir_x * sin(player->rot_speed)
-			+ player->dir_y * cos(player->rot_speed);
-		player->plane_x = player->plane_x * cos(player->rot_speed)
-			- player->plane_y * sin(player->rot_speed);
-		player->plane_y = old_plane_x * sin(player->rot_speed)
-			+ player->plane_y * cos(player->rot_speed);
-		ft_raycasting(game);
-	}
+		ft_turn_left_right(game, game->player, 1);
 	mlx_mouse_move(game->win, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 	return (0);
 }
